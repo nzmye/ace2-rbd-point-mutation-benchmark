6c3af2431b566554d6cba445a43d136b8ef4fb88
nzmye <nzmye.arslan01@gmail.com>
2021-12-24 15:20:46 +0300

    try get pdb url  name

--- visualize/js/make_table.js
+++ visualize/js/make_table.js
@@ -112,7 +112,7 @@ table = new Tabulator("#models-table", {
     let pdbname = myArray[1] + "_ " + columnname + ".pdb"
     let pdburl = "models/" + rowname + "/" + pdbname;
 
-    alert(`The cell has a url of:${pdburl}`); //display the cells refer a url
+    alert(`The cell has a url of:${myArray}`); //display the cells refer a url
 
     loadMolecule(stage, pdburl)
 
