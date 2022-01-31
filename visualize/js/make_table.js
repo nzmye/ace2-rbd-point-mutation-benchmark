421fd68cd9a3cd8478f8a099fb8b63bed1cf5085
nzmye <nzmye.arslan01@gmail.com>
2022-01-31 14:17:30 +0300

    changed color when cell clicked

--- visualize/js/make_table.js
+++ visualize/js/make_table.js
@@ -135,14 +135,14 @@ table = new Tabulator("#models-table", {
 
     var columnname = cell.getColumn().getField()
     var rowname = cell.getRow().getData().Names
-    // alert(`The cell has a value of:${row.getData().Names}${column.getField()}${cell.getValue()}`); //display the cells value
+    alert(`The cell has a value of:${row.getData().Names}${column.getField()}${cell.getValue()}`); //display the cells value
     //alert(`The cell has a value of:${rowname}${columnname}${cell.getValue()}`); //display the cells value
 
     let myArray = rowname.split("_");
     let modelname = rowname.replace("_", "-")
     let pdbname = `${myArray[1]}_${columnname}.pdb`
     let pdburl = `models/${modelname}/${pdbname}`
-    cell.getElement().style.backgroundColor = "#00000";
+    cell.getElement().style.backgroundColor = "#FFFFFF";
     //alert(`The cell has a url of:${pdburl}`); //display the cells refer a url
     removeMolecule(stage, pdburl)
 
