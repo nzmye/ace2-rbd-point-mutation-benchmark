e43801d2d24c65f582fa9e8a68236cd2ebfc46d8
nzmye <nzmye.arslan01@gmail.com>
2022-04-13 22:51:24 +0300

    added all models

--- visualize/js/make_table.js
+++ visualize/js/make_table.js
@@ -123,7 +123,7 @@ table = new Tabulator("#models-table", {
     let myArray = rowname.split("_");
     let modelname = rowname.replace("_", "-")
     let pdbname = `${myArray[1]}_${columnname}.pdb` 
-     let pdburl = `https://github.com/CSB-KaracaLab/ace2-rbd-point-mutation-benchmark/tree/main/SARS_CoV-2-generated_mutant_models/${modelname}/${pdbname}`
+    let pdburl = `models/${modelname}/${pdbname}`
     cell.getElement().style.backgroundColor = "#e68a00";
     //alert(`The cell has a url of:${pdburl}`); //display the cells refer a url
      loadMolecule(stage, pdburl)
@@ -139,7 +139,7 @@ table = new Tabulator("#models-table", {
     let myArray = rowname.split("_");
     let modelname = rowname.replace("_", "-")
     let pdbname = `${myArray[1]}_${columnname}.pdb`
-    let pdburl = `https://github.com/CSB-KaracaLab/ace2-rbd-point-mutation-benchmark/tree/main/SARS_CoV-2-generated_mutant_models/${modelname}/${pdbname}`
+    let pdburl = `models/${modelname}/${pdbname}`
     cell.getElement().style.backgroundColor = "#FFFFFF";
     //alert(`The cell has a url of:${pdburl}`); //display the cells refer a url
     removeMolecule(stage, pdburl)
